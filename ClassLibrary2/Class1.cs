45ff183eece2e27645a2de3626f0bf32dac14f8c
олина попа < @DESKTOP-HI2AFKM>
2022-11-02 00:09:02 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    
+    
+        public delegate void AccountHandler(string message);
+        public class Account
+        {
+            public int sum;
+            public string Fio;
+            AccountHandler taken;
+            public Account(int sum, string fio)
+            {
+                this.sum = sum;
+                Fio = fio;
+            }
+            
+            public void RegisterHandler(AccountHandler del)
+            {
+                taken += del;
+            }
+           
+            public void UnregisterHandler(AccountHandler del)
+            {
+                taken -= del; 
+            }
+            public void Add(int sum) => this.sum += sum;
+            public void Take(int sum)
+            {
+                if (this.sum >= sum)
+                {
+                    this.sum -= sum;
+                    taken?.Invoke($"Со счета списано {sum} у.е.");
+                }
+                else
+                    taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+            }
+        }
+    
+}
---
+++ Delegate/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary2;
+
+namespace Delegate
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Account account;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            account = new Account (Convert.ToInt32(textBox2.Text), textBox1.Text);
+            listBox1.Items.Add($"Создать аккаунт - {account.Fio} Баланс - {account.sum}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            account.Add(Convert.ToInt32(textBox3.Text));
+            listBox1.Items.Add($"Положить сумму - {textBox3.Text} Баланс - {account.sum}");
+        }
+    }
+}
